93f8cc9bfe18c19eef04aee93d68cd1c977f0de9
Searica <143636061+searica@users.noreply.github.com>
2025-01-28 09:42:35 -0800

    Add configs

--- WatchWhereYouStab.cs
+++ WatchWhereYouStab.cs
@@ -6,6 +6,10 @@ using HarmonyLib;
 using System.Reflection;
 using UnityEngine;
 using Logging;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System;
 
 namespace VerticalAttacks
 {
@@ -19,7 +23,9 @@ namespace VerticalAttacks
         
         public static WatchWhereYouStab Instance;
         private const string MainSection = "Global";
-        internal ConfigEntry<float> MaxAngle;       
+        internal ConfigEntry<float> MaxAngle;
+        internal ConfigEntry<bool> FixedHeight;
+        internal ConfigEntry<float> AngleCorrection;
 
         public void Awake()
         {
@@ -44,6 +50,25 @@ namespace VerticalAttacks
                 65f,
                 "Angle you can aim melee attacks up or down by. This is only applied to attacks that currently have a smaller maximum angle than the configured value.",
                 new AcceptableValueRange<float>(5f, 90f),
+                synced: false
+            );
+
+            FixedHeight = Config.BindConfigInOrder(
+                MainSection,
+                "Fixed Attack Height",
+                false,
+                "Different melee attacks originate from different positions based on their vanilla animations. Enable this to make all melee attacks " +
+                "originate from the same position to make aiming more consistent.",
+                synced: false
+            );
+
+            AngleCorrection = Config.BindConfigInOrder(
+                MainSection,
+                "Angle Correction",
+                0f,
+                "Angle to apply as a correction to your current look direction. This can be used to offset the way the camera is usually pointing downwards. " +
+                "If positive then attacks will be aimed above the current camera direction, if negative they will be aimed below.",
+                new AcceptableValueRange<float>(-30f, 30f),
                 synced: false
             );
 
@@ -79,6 +104,73 @@ namespace VerticalAttacks
 
             // Sets max angle limit for RotateTowards method
             __instance.m_maxYAngle = Mathf.Max(__instance.m_maxYAngle, WatchWhereYouStab.Instance.MaxAngle.Value);
+
+
+            if (WatchWhereYouStab.Instance.FixedHeight.Value)
+            {
+                __instance.m_attackHeight = 1f;
+            }
+        }
+
+        /// <summary>
+        ///     Modify calcuation of the melee attack direction by replacing the call to Attack.GetMeleeAttackDir
+        ///     with a call to AttachPatch.GetMeleeAttackDirection.
+        /// </summary>
+        /// <param name="instructions"></param>
+        /// <returns></returns>
+        [HarmonyTranspiler]
+        [HarmonyPatch((typeof(Attack)), nameof(Attack.DoMeleeAttack))]
+        private static IEnumerable<CodeInstruction> DoMeleeAttack_Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            CodeMatch[] targetCodeMatches =
+            [
+                new(OpCodes.Call, AccessTools.Method(typeof(Attack), nameof(Attack.GetMeleeAttackDir)))
+            ];
+
+            CodeInstruction replacementCode = new(
+                OpCodes.Call,
+                AccessTools.Method(
+                    typeof(AttackPatch),
+                    nameof(GetMeleeAttackDirection)
+                )
+            );
+
+
+            return new CodeMatcher(instructions)
+                .Start()
+                .MatchStartForward(targetCodeMatches)
+                .ThrowIfNotMatch("Failed to match code in Attack.DoMeleeAttack!")
+                .SetInstructionAndAdvance(replacementCode)
+                .ThrowIfInvalid("Failed to patch Attack.DoMeleeAttack!")
+                .InstructionEnumeration();
+        }
+
+        //[Error: Unity Log] InvalidProgramException: Invalid IL code in (wrapper dynamic-method) Attack:DMD<Attack::DoMeleeAttack>(Attack): IL_0018: call      0x00000005
+        
+        /// <summary>
+        ///     Apply the angle correction if needed.
+        /// </summary>
+        /// <param name="attack"></param>
+        /// <param name="originJoint"></param>
+        /// <param name="attackDir"></param>
+        private static void GetMeleeAttackDirection(Attack attack, out Transform originJoint, out Vector3 attackDir)
+        {
+            originJoint = attack.GetAttackOrigin();
+            Vector3 forward = attack.m_character.transform.forward;
+            Vector3 aimDir = attack.m_character.GetAimDir(originJoint.position);
+            aimDir.x = forward.x;
+            aimDir.z = forward.z;
+            aimDir.Normalize();
+
+            float angleCorrection = WatchWhereYouStab.Instance.AngleCorrection.Value;
+            if (angleCorrection != 0f) 
+            {
+                Vector3 targetDir = Mathf.Sign(angleCorrection) * Vector3.up;
+                aimDir = Vector3.RotateTowards(aimDir, targetDir, Mathf.Deg2Rad * Mathf.Abs(angleCorrection), 0.0f);
+            }
+                        
+            attackDir = Vector3.RotateTowards(attack.m_character.transform.forward, aimDir, Mathf.Deg2Rad * attack.m_maxYAngle, 10f);
+            Log.LogInfo("Doing the thing!");
         }
     }
 }
